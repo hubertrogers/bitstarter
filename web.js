89c4024e5d83a49c20844705192104a4a8243d39
Ubuntu <ubuntu@ip-172-31-47-248.us-west-2.compute.internal>
2013-07-10 22:00:01 +0000

    hi

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 app.get('/', function(request, response) {
 
-  fs.readFileSync('index.html', 'utf8', function (err, data) {
+    fs.readFileSync('index.html', {'encoding':'utf8'}, function (err, data) {
       if (err) throw err;
       response.send(data);
   });
